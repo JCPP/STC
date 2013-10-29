5c04b614a22c872a7d32b1704cd01056a1166814
Davide Pastore <pasdavide@gmail.com>
2013-10-29 19:18:38 +0100

    Lettura dati demo.

--- web/app/router/arduino/ricezione.php
+++
@@ -1,2 +0,0 @@
-<?php
-//$allPostVars = $app->request->post();
--- web/app/router/router.php
+++ web/app/router/router.php
@@ -1,8 +1,40 @@
 <?php
 
-require 'admin/visualizzazione.php';
+//Home Page
+$app->get('/', function () use ($app) {
+	$app->render('index.twig');
+})->name("HomePage");
+
+//Visualizzazione dei dati
+$app->get('/visualizza', function () use ($app) {
+	$app->render('visualizza.twig');
+})->name("Visualizza");
 
 //Pagina non trovata
 $app->notFound(function () use ($app) {
 	$app->redirect('/');
 });
+
+//Ricevi dati
+$app->post('/ricevi', function () use ($app) {
+	$postVars = $app->request()->post();
+	$temperatura = "";
+	$umidita = "";
+	$pressione = "";
+	
+	if(isset($_POST['t'])){
+		$temperatura = $_POST['t'];
+	}
+	
+	if(isset($_POST['u'])){
+		$umidita = $_POST['u'];
+	}
+	
+	if(isset($_POST['p'])){
+		$pressione = $_POST['p'];
+	}
+	
+	echo ("Temperatura: " . $temperatura . "/n");
+	echo ("Umidità: " . $umidita . "/n");
+	echo ("Pressione: " . $pressione . "/n");
+})->name("Ricevi");
